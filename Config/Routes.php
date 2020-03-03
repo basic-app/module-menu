1d36f2d606197845a4f02bf71c53a851a727744f
basic-app <dev@basic-app.com>
2020-03-03 21:16:02 +0300

    n/a

--- Config/Routes.php
+++ Config/Routes.php
@@ -1,7 +1,13 @@
 <?php
 
 $routes->add('admin/menu', '\BasicApp\Menu\Controllers\Admin\Menu::index');
+$routes->add('{locale}/admin/menu', '\BasicApp\Menu\Controllers\Admin\Menu::index');
+
 $routes->add('admin/menu/(:segment)', '\BasicApp\Menu\Controllers\Admin\Menu::$1');
+$routes->add('{locale}/admin/menu/(:segment)', '\BasicApp\Menu\Controllers\Admin\Menu::$1');
 
 $routes->add('admin/menu-item', '\BasicApp\Menu\Controllers\Admin\MenuItem::index');
+$routes->add('{locale}/admin/menu-item', '\BasicApp\Menu\Controllers\Admin\MenuItem::index');
+
 $routes->add('admin/menu-item/(:segment)', '\BasicApp\Menu\Controllers\Admin\MenuItem::$1');
+$routes->add('{locale}/admin/menu-item/(:segment)', '\BasicApp\Menu\Controllers\Admin\MenuItem::$1');
