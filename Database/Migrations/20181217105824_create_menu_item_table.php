4286a899c96bb86ea2cbe28f5d643b253895b2e0
basic-app <dev@basic-app.com>
2020-03-04 08:49:12 +0300

    n/a

--- Database/Migrations/20181217105824_create_menu_item_table.php
+++ Database/Migrations/20181217105824_create_menu_item_table.php
@@ -11,6 +11,8 @@ class Migration_create_menu_item_table extends \BasicApp\Core\Migration
 
     public $tableName = 'menu_item';
 
+    public $menuTableName = 'menu';
+
     public function up()
     {
         $this->forge->addField([
@@ -31,7 +33,7 @@ class Migration_create_menu_item_table extends \BasicApp\Core\Migration
 
         $this->forge->addKey(['item_uid', 'item_menu_id'], false, true);
 
-        $this->forge->addForeignKey('item_menu_id', 'menu', 'menu_id', 'RESTRICT', 'RESTRICT');
+        $this->forge->addForeignKey('item_menu_id', $this->menuTableName, 'menu_id', 'RESTRICT', 'RESTRICT');
 
         $this->forge->createTable($this->tableName, false);
     }
